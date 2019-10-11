83454a0853775afd3820e81c6765ebed60999fb8
grayluker05 <graysonluker@gmail.com>
2019-10-11 10:41:26 -0600

    second commit

---
+++ db/add_comment.sql
@@ -0,0 +1,5 @@
+insert into posts (topic, text)
+values ($1, $2);
+
+select * 
+from posts;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
+const ctrl = require('./controller')
 
 const app = express()
 app.use(express.json())
@@ -9,8 +10,14 @@ const {SERVER_PORT, CONNECTION_STRING} = process.env
 
 massive(CONNECTION_STRING)
     .then(db => {
-        app.set(db)
-        app.listen(SERVER_PORT, console.log(`Running on port: ${SERVER_PORT}`))
+        app.set('db', db)
         console.log('db connected')
     })
     .catch(err => console.log(err))
+    
+    //endpoints
+    app.get('/api/comments', ctrl.getComments)
+    app.post('/api/comments', ctrl.addComment)
+    app.put('/api/comments/:id', ctrl.updateComment)
+
+    app.listen(SERVER_PORT, console.log(`Running on port: ${SERVER_PORT}`))
